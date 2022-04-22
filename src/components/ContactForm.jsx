54c2dbb46d60956432c70111a7002c364fba0319
Sajadmhy <sajad.mahyaei@gmail.com>
2022-04-22 08:07:39 -0400

    Add contactfrom component

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,37 @@
+function UserForm() {
+    return (
+        <div>
+            <form>
+                <div>
+                    <h3>Contact Form</h3>
+                </div>
+                <div>
+                    <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    />
+                </div>
+                <div>
+                    <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    />
+                </div>
+                <div>
+                    <input
+                    type="number"
+                    name="phonenumber"
+                    placeholder="Phone Number"
+                    />
+                </div>
+                <div>
+                    <button>Submit Contact</button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default UserForm;
